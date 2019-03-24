faec8dbe9c1e546401befa5dcc1bf89b91927b7b
luxinlx <48483573+luxinlx@users.noreply.github.com>
2019-03-24 23:20:27 +0800

    Add files via upload

---
+++ 实验二代码/2-4pid.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<unistd.h>
+
+int main()
+{
+      pid_t p,p2,p3,p4,p5;
+      int flag=0;
+      while((p=fork())==-1);
+      if(!p)
+      {
+            while((p2=fork())==-1);
+            if(!p2)
+            {
+                  while(1){
+                  printf("进程P4的pid为%d，父进程为p2,id为%d\n",getpid(),getppid());
+                  }
+            }
+            else if(p2>0)
+            {
+                  while(1){
+                  printf("进程p2的pid为%d，父进程为p，id为%d\n",getpid(),getppid());
+                  }
+            }
+            while((p2=fork())==-1);
+            if(!p2)
+            {
+                  while(1){
+                  printf("进程p5的pid为%d，父进程为p2，id为%d\n",getpid(),getppid());
+                  }
+            }
+             else
+            {
+                   ;
+            }
+       }
+      else if(p>0)
+      {
+            while(1){
+            printf("进程p为父进程,id为%d,它的父进程id为%d\n",getpid(),getppid());
+                 }
+   }
+      while((p=fork())==-1);
+      if(!p)
+      {
+            while(1){
+            printf("进程p3的id为%d,父进程为p，id为%d\n",getpid(),getppid());
+            }
+      }
+      else
+      {
+            ;
+      }
+      return 0;
+}
